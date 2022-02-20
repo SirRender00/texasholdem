23629e2748c1649ce48fbfbe6ca5996c01eca3eb
SirRender00 <32499767+SirRender00@users.noreply.github.com>
2022-02-19 21:58:53 -0800

    Adds HistoryModuleImport exceptions and aliases (#31)
    - Adds the HistoryModuleImport exception which is thrown whenever an
      invalid history file tries to be imported / played.
    - Adds many usefule aliases for all the modules

--- texasholdem/game/__init__.py
+++ texasholdem/game/__init__.py
@@ -2,14 +2,14 @@
 The core game package
 """
 
-from action_type import ActionType
-from game import TexasHoldEm, GameState
-from hand_phase import HandPhase
-from history import (History,
-                     FILE_EXTENSION,
-                     HistoryImportError,
-                     SettleHistory,
-                     BettingRoundHistory,
-                     PrehandHistory,
-                     PlayerAction)
-from player_state import PlayerState
+from texasholdem.game.action_type import ActionType
+from texasholdem.game.game import TexasHoldEm, GameState
+from texasholdem.game.hand_phase import HandPhase
+from texasholdem.game.history import (History,
+                                      FILE_EXTENSION,
+                                      HistoryImportError,
+                                      SettleHistory,
+                                      BettingRoundHistory,
+                                      PrehandHistory,
+                                      PlayerAction)
+from texasholdem.game.player_state import PlayerState
