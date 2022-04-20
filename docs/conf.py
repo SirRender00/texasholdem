010c22f60633c91ce3d50285cd5c0cb98fdb09d6
Evyn Machi <evyn.machi@gmail.com>
2022-04-19 21:04:32 -0700

    bump to 0.7.1

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2022, Evyn Machi'
 author = 'Evyn Machi'
 
 # The full version, including alpha/beta/rc tags
-release = 'v0.7.0'
+release = 'v0.7.1'
 
 
 # -- General configuration ---------------------------------------------------
