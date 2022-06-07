0e7c425e41042eed2475907851760c1dc778c731
Evyn Machi <32499767+SirRender00@users.noreply.github.com>
2022-06-06 23:46:49 -0700

    bump to 0.5.3 (#115)

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2022, Evyn Machi'
 author = 'Evyn Machi'
 
 # The full version, including alpha/beta/rc tags
-release = 'v0.5.2'
+release = 'v0.5.3'
 
 
 # -- General configuration ---------------------------------------------------
