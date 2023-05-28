03f2f63b02eeb7d3d4861ab8b9d7006e87fb2397
Lucas Colas <51634013+LucasColas@users.noreply.github.com>
2023-05-28 16:49:25 +0200

    Fix trailing whitespace

--- texasholdem/game/game.py
+++ texasholdem/game/game.py
@@ -293,13 +293,13 @@ class TexasHoldEm:
         self._action = None, None
         self._hand_gen = None
         
-    @property        
+    @property
     def action(self) -> Tuple[ActionType, Optional[int]]:
         """
         Get the last action (check, call, raise of flop).
-        
         """
         return self._action
+
 
     def _prehand(self):
         """
